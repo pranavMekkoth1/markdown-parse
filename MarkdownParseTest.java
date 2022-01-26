78dc26e16bf08a143be9d0462e95817360aad606
pranavMekkoth1 <pmekkoth@ucsd.edu>
2022-01-26 15:36:33 -0800

    junit added

---
+++ MarkdownParseTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+//above commands imports libraries
+
+
+public class MarkdownParseTest {
+    @Test
+    //defines method below as a junit test
+    public void addition() {
+        assertEquals(2, 1 + 1);//checks if input results in expected output
+        //equivalent to the value of the second param
+    }
+}
